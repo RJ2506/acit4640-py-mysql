663acec19bd0cca65ea9b582eddf86850362309f
rj <rjgayem@gmail.com>
2022-11-21 11:48:30 -0800

    added file

--- backend/start.sh
+++ backend/start.sh
@@ -7,6 +7,6 @@ echo "This runs before the web service is launched in the docker container"
 sed -i "s|___MYSQL_HOST___|${MYSQL_HOST}|" /app/backend.conf
 /app/wait-for-it.sh -h ${MYSQL_HOST} -p ${MYSQL_PORT} -t 30 --
 
-envsubst < /app/backend.conf > /app/backend.conf
+envsubst < /app/backend.conf.template > /app/backend.conf
 
 exec /app/.local/bin/gunicorn wsgi:app -b 0.0.0.0:8000
