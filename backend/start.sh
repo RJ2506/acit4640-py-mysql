66edc63445c40c1410ce8a0150889c039cd78083
rj <rjgayem@gmail.com>
2022-11-21 11:38:33 -0800

    move the backend.conf

--- backend/start.sh
+++ backend/start.sh
@@ -4,9 +4,9 @@ set -ex
 
 
 echo "This runs before the web service is launched in the docker container"
-envsubst < /app/backend.conf > /app/backend.conf
 sed -i "s|___MYSQL_HOST___|${MYSQL_HOST}|" /app/backend.conf
 /app/wait-for-it.sh -h ${MYSQL_HOST} -p ${MYSQL_PORT} -t 30 --
 
+envsubst < /app/backend.conf > /app/backend.conf
 
 exec /app/.local/bin/gunicorn wsgi:app -b 0.0.0.0:8000
