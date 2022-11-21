ce958f8ea8b1bc64da28c754b0cf3cb4f8ae2851
rj <rjgayem@gmail.com>
2022-11-21 11:15:56 -0800

    fix the path

--- backend/start.sh
+++ backend/start.sh
@@ -4,9 +4,9 @@ set -ex
 
 
 echo "This runs before the web service is launched in the docker container"
-sed -i "s|___MYSQL_HOST___|${MYSQL_HOST}|" /app/src/backend.conf
-/app/src/wait-for-it.sh -h ${MYSQL_HOST} -p ${MYSQL_PORT} -t 30 --
+envsubst < /app/backend.conf > /app/backend.conf
+sed -i "s|___MYSQL_HOST___|${MYSQL_HOST}|" /app/backend.conf
+/app/wait-for-it.sh -h ${MYSQL_HOST} -p ${MYSQL_PORT} -t 30 --
 
 
-envsubst < /app/src/backend.conf.template > /app/src/backend.conf
 exec /app/.local/bin/gunicorn wsgi:app -b 0.0.0.0:8000
